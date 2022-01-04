f62cae649024217f4c507a47ce25fd709b6eeccc
zhangws <zhangws@253.com>
2022-01-04 11:18:25 +0800

    测试Maven

---
+++ testMaven/src/main/java/com/example/testmaven/TestMavenApplication.java
@@ -0,0 +1,16 @@
+package com.example.testmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author EDY
+ */
+@SpringBootApplication
+public class TestMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMavenApplication.class, args);
+    }
+
+}
